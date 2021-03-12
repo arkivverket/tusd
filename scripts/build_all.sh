3bde4ce103867688fa3d1eb774d89ae7675f4232
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-03-12 12:47:10 +0100

    Test build artifacts

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -6,11 +6,11 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${__dir}/build_funcs.sh"
 
-compile linux   386
+# compile linux   386
 compile linux   amd64
-compile linux   arm
-compile linux   arm64
-compile darwin  386
+# compile linux   arm
+# compile linux   arm64
+# compile darwin  386
 compile darwin  amd64
-compile windows 386   .exe
-compile windows amd64 .exe
+# compile windows 386   .exe
+# compile windows amd64 .exe
